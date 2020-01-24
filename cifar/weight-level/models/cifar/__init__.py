f328cca007ffb8cc1c38c8507acddafad7f73a68
Eric-mingjie <sunmj15@gmail.com>
2020-01-23 19:54:43 -0500

    Remove unnecessary import

--- cifar/weight-level/models/cifar/__init__.py
+++ cifar/weight-level/models/cifar/__init__.py
@@ -64,7 +64,5 @@ Densenet-161             22.35           6.20
 from .alexnet import *
 from .vgg import *
 from .resnet import *
-from .resnext import *
-from .wrn import *
 from .preresnet import *
 from .densenet import *
